a9d9483849b0db10e37da4e25b7e14d1867c7b72
Lisa Tetteroo <lisa.tetteroo.foto@gmail.com>
2020-03-13 16:43:31 +0100

    stuck at printing cards, duurt te lang?

--- Card.java
+++ Card.java
@@ -4,27 +4,44 @@ package nl.workingtalent.weekopdracht;
 Doel: Het aanmaken van de 52 kaarten. elke kaart heeft een puntenwaarde, kleur, getal.
 
 getal en puntenwaarde.
-aas = 1 of 11 (in de doelstelling 2 10)
-boer, vrouw, heer = 10
+aas = 1 of 11 punten  (in de doelstelling 2 10) (naam --> int = 1)
+boer, vrouw, heer = 10 punten (naam --> int boer = 11, vrouw = 12, heer = 13)
 2 t/m 10 = waarde kaart
 
-kleuren(suit): H, S, K, R voor harten, schoppen, klaver, ruiten.
+kleuren(suit): S=0,H=1,R=2,K=3 (schoppen, harten, ruiten, klaveren)
  */
 
 public class Card {
-    String value;
-    String suit;
-    int pointValue;
+    private int value;
+    private int suit;
+    private int pointValue;
+
+    public static final int SCHOPPEN = 0, HARTEN = 1, RUITEN = 2, KLAVER = 3;
+    public static final int AAS = 1, BOER = 11, VROUW = 12, HEER = 13;
 
 
-    public Card (int i) {
-        this.value = Integer.toString(i);
+
+    public Card (int value, int suit) {
+        this.value = value;
+        this.suit = suit;
     }
 
+    public int getValue() {
+        return value;
+    }
+
+    public int getSuit() {
+        return suit;
+    }
+/*
     @Override
     public String toString() {
         return "Card{" +
-                "cardnr='" + value + '\'' +
+                "value=" + value +
+                ", suit=" + suit +
+                ", pointValue=" + pointValue +
                 '}';
     }
+
+ */
 }
